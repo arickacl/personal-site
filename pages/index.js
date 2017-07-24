ae24bb83cb1f0a81f53123b2483c24ae3c0e72cc
Hayden Luckenbach <hluckenb@gmail.com>
2017-07-23 20:03:36 -0500

    bootstrap and homepage

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ss from '../bootstrap/bootstrap.scss'
+
+export const IndexPage = () => (
+  <div className={ss.container}>
+    Hi
+  </div>
+)
+
+export default IndexPage
